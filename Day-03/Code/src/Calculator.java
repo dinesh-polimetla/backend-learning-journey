0f84d32712574d4b3a58f98fa1014b46d238a709
Dinesh <dineshpolimetla25@gmail.com>
2026-07-23 20:23:08 +0530

    optimized a little bit

--- Day-03/Code/src/Calculator.java
+++ Day-03/Code/src/Calculator.java
@@ -9,13 +9,13 @@ public class Calculator {
         System.out.print("Enter number n2: ");
         int n2 =  scanner.nextInt();
 
-        System.out.println("The sum of two numbers is : " + addNums(n1,n2));
+        System.out.println("The sum of two numbers is : " + add(n1,n2));
         System.out.println("The difference between two numbers is : " + subtract(n1,n2));
         System.out.println("The product of two numbers is : " + multiply(n1,n2));
         System.out.println("The division of two numbers is : " + divide(n1,n2));
         scanner.close();
     }
-    public static int addNums(int a, int b) {
+    public static int add(int a, int b) {
         return a+b;
     }
     public static int subtract(int a, int b){
